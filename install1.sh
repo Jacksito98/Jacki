483964e63b7769fde5570c7b3b78453a007eb50d
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2022-01-24 15:06:56 -0400

    Add files via upload

--- install1.sh
+++ install1.sh
@@ -19,10 +19,10 @@ npm i hxz-api
 npm i
 echo -e '\e[1;32m
 Descarga Completada al 100%
-Creador: Thechoute
-Numero: +18299897014
-Instagram: the_choute_
-Youtube: https://www.youtube.com/c/thechout
+Creador: Jacksito98
+Numero: +64277772777
+Instagram: Jack_paymon
+Youtube: https://www.youtube.com/c/Jacksito98
 Escribe node thechoute para iniciar el bot
 En caso que el bot presente algun problema, comunicate con el creador su numero esta mas arriba
 '
